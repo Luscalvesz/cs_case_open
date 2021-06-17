c221cb16b80f34d927c541a4ba08c5768d021702
lucas alves <lucas.pereira@codeby.com.br>
2021-06-17 19:07:53 -0300

    first commit, import jquery and slick

---
+++ assets/script/carrossel.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function () {
+    $('#carousel').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+    });
+});
