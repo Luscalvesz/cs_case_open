3a29d051f89e9733e1fceeaf5f1f1e4a731a746f
Lucas Pereira <lucas.a.pereira05@gmail.com>
2021-07-11 13:02:03 -0300

    fixing inventory

---
+++ assets/script/script.js
@@ -0,0 +1,328 @@
+var boxes = {
+    "snakebite": [
+        {"key": "key.png"},
+        {"boxName": "snakebite"},
+        { "name": "In Living Color", "weapon": "M4A4", "rarity": "hidden", "img": "M4a4-in-living-color.png" },
+        { "name": "The Traitor", "weapon": "USP-S", "rarity": "hidden", "img": "Usp-s-the-traitor.png" },
+        { "name": "XOXO", "weapon": "XM1014", "rarity": "exotic", "img": "Xm1014-xoxo.png" },
+        { "name": "Food Chain", "weapon": "MP9", "rarity": "exotic", "img": "Mp9-food-chain.png" },
+        { "name": "Chromatic Aberration", "weapon": "Galil AR", "rarity": "exotic", "img": "Galil-ar-chromatic-aberration.png" },
+        { "name": "Slate", "weapon": "AK-47", "rarity": "remarkable", "img": "Ak-47-slate.png" },
+        { "name": "Trigger Discipline", "weapon": "Desert Eagle", "rarity": "remarkable", "img": "Desert-eagle-trigger-discipline.png" },
+        { "name": "dev_texture", "weapon": "Negev", "rarity": "remarkable", "img": "Negev-dev-texture.png" },
+        { "name": "Button Masher", "weapon": "MAC-10", "rarity": "remarkable", "img": "Mac-10-button-masher.png" },
+        { "name": "Cyber Shell", "weapon": "P250", "rarity": "remarkable", "img": "P250-cyber-shell.png" },
+        { "name": "Junkyard", "weapon": "R8 Revolver", "rarity": "industrial level", "img": "R8-revolver-junk-yard.png" },
+        { "name": "Heavy Metal", "weapon": "SG 553", "rarity": "industrial level", "img": "Sg-553-heavy-metal.png" },
+        { "name": "Oscillator", "weapon": "UMP-45", "rarity": "industrial level", "img": "Ump-45-oscillator.png" },
+        { "name": "O.S.I.P.R", "weapon": "M249", "rarity": "industrial level", "img": "M249-osipr.png" },
+        { "name": "Circaetus", "weapon": "CZ75-Auto", "rarity": "industrial level", "img": "Cz75-auto-circaetus.png" },
+        { "name": "Clear Polymer", "weapon": "Glock-18", "rarity": "industrial level", "img": "Glock-18-clear-polymer.png" },
+        { "name": "Windblown", "weapon": "Nova", "rarity": "industrial level", "img": "Nova-windblown.png" }
+    ],
+    "falchion": [
+        {"key": "key.png"},
+        {"boxName": "falchion"},
+        { "name": "faca caraio", "weapon": "Falchion", "rarity": "hidden", "img": "falchion-knife.png" },
+        { "name": "Hyper Beast", "weapon": "Awp", "rarity": "hidden", "img": "awp-hyper-beast.png" },
+        { "name": "Aquamarine Revenge", "weapon": "AK-47", "rarity": "hidden", "img": "ak-47-aquamarine-revenge.png" },
+        { "name": "Cyrex", "weapon": "SG553", "rarity": "exotic", "img": "sg553-cyrex.png" },
+        { "name": "Nemesis", "weapon": "MP7", "rarity": "exotic", "img": "mp7-nemesis.png" },
+        { "name": "Yellow Jacket", "weapon": "CZ75-Auto", "rarity": "exotic", "img": "cz75auto-yellow-jacket.png" },
+        { "name": "Handgun", "weapon": "P2000", "rarity": "remarkable", "img": "p2000-handgun.png" },
+        { "name": "Loudmouth", "weapon": "Negev", "rarity": "remarkable", "img": "negev-loudmouth.png" },
+        { "name": "Ruby Poison Dart", "weapon": "MP9", "rarity": "remarkable", "img": "mp9-ruby-poison-dart.png" },
+        { "name": "Evil Daimyo", "weapon": "M4A4", "rarity": "remarkable", "img": "m4a4-evil-daimyo.png" },
+        { "name": "Neural Net", "weapon": "Famas", "rarity": "remarkable", "img": "famas-neural-net.png" },
+        { "name": "Torque", "weapon": "USP-S", "rarity": "industrial level", "img": "usps-torque.png" },
+        { "name": "Riot", "weapon": "UMP-45", "rarity": "industrial level", "img": "ump45-riot.png" },
+        { "name": "Elite Build", "weapon": "P-90", "rarity": "industrial level", "img": "p90-elite-build.png" },
+        { "name": "Ranger", "weapon": "nova", "rarity": "industrial level", "img": "nova-ranger.png" },
+        { "name": "Bunsen Burner", "weapon": "Glock-18", "rarity": "industrial level", "img": "glock18-bunsen-burner.png" },
+        { "name": "Rocket Pop", "weapon": "Galil AR", "rarity": "industrial level", "img": "galilar-rocket-pop.png" },
+    ],
+    "hidra": [
+        {"key": "key.png"},
+        {"boxName": "hidra"},
+        { "name": "Oni Taiji", "weapon": "AWP", "rarity": "hidden", "img": "awp-oni-taiji.png" },
+        { "name": "Hyper Beast", "weapon": "Five-SeveN", "rarity": "hidden", "img": "five-seven-hyper-beast.png" },
+        { "name": "Hellfire", "weapon": "M4A4", "rarity": "hidden", "img": "m4a4-hellfire.png" },
+        { "name": "Sugar Rush", "weapon": "Galil AR", "rarity": "exotic", "img": "galilar-sugar-rush.png" },
+        { "name": "Cobra Strike", "weapon": "Dual Berettas", "rarity": "exotic", "img": "dual-berettas-cobra-strike.png" },
+        { "name": "Death's Head", "weapon": "SSG 08", "rarity": "remarkable", "img": "ssg08-deaths-head.png" },
+        { "name": "Death Grip", "weapon": "P90", "rarity": "remarkable", "img": "p90-death-grip.png" },
+        { "name": "Red Rock", "weapon": "P250", "rarity": "remarkable", "img": "p250-red-rock.png" },
+        { "name": "Woodsman", "weapon": "P2000", "rarity": "remarkable", "img": "p2000-woodsman.png" },
+        { "name": "Orbit Mk01", "weapon": "AK-47", "rarity": "remarkable", "img": "ak-47-orbit-mk01.png" },
+        { "name": "Metal Flowers", "weapon": "UMP-45", "rarity": "industrial level", "img": "ump45-metal-flowers.png" },
+        { "name": "Cut Out", "weapon": "Tec-9", "rarity": "industrial level", "img": "tec9-cut-out.png" },
+        { "name": "Hard Water", "weapon": "MAG-7", "rarity": "industrial level", "img": "mag7-hard-water.png" },
+        { "name": "Aloha", "weapon": "MAC-10", "rarity": "industrial level", "img": "mac10-aloha.png" },
+        { "name": "Briefing", "weapon": "M4A1-S", "rarity": "industrial level", "img": "m4a1s-briefing.png" },
+        { "name": "Macabre", "weapon": "FAMAS", "rarity": "industrial level", "img": "famas-macabre.png" },
+        { "name": "Blueprint", "weapon": "USP-S", "rarity": "industrial level", "img": "usps-blueprint.png" },
+    ],
+    "gamma": [
+        {"key": "key.png"},
+        {"boxName": "gamma"},
+        { "name": "Mecha Industries", "weapon": "M4A1-S", "rarity": "hidden", "img": "m4a1s-mecha-industries.png" },
+        { "name": "Wasteland Rebel", "weapon": "Glock-18", "rarity": "hidden", "img": "glock18-wasteland-rebel.png" },
+        { "name": "Bloodsport", "weapon": "SCAR-20", "rarity": "exotic", "img": "scar20-bloodsport.png" },
+        { "name": "Imperial Dragon", "weapon": "P2000", "rarity": "exotic", "img": "p2000-imperial-dragon.png" },
+        { "name": "Desolate Space", "weapon": "M4A4", "rarity": "exotic", "img": "m4a4-desolate-space.png" },
+        { "name": "Limelight", "weapon": "Sawed-Off", "rarity": "remarkable", "img": "sawedoff-limelight.png" },
+        { "name": "Reboot", "weapon": "R8 Revolver", "rarity": "remarkable", "img": "r8revolver-reboot.png" },
+        { "name": "Chopper", "weapon": "P90", "rarity": "remarkable", "img": "p90-chopper.png" },
+        { "name": "Phobos", "weapon": "AWP", "rarity": "remarkable", "img": "awp-phobos.png" },
+        { "name": "Aristocrat", "weapon": "AUG", "rarity": "remarkable", "img": "aug-aristocrat.png" },
+        { "name": "Ice Cap", "weapon": "Tec-9", "rarity": "industrial lever", "img": "tec9-ice-cap.png" },
+        { "name": "Aerial", "weapon": "SG 553", "rarity": "industrial level", "img": "sg553-aerial.png" },
+        { "name": "Harvester", "weapon": "PP-Bizon", "rarity": "industrial level", "img": "ppbizon-harvester.png" },
+        { "name": "Exo", "weapon": "Nova", "rarity": "industrial level", "img": "nova-exo.png" },
+        { "name": "Carnivore", "weapon": "MAC-10", "rarity": "industrial level", "img": "mac10-carnivore.png" },
+        { "name": "Violent Daimyo", "weapon": "Five-SeveN", "rarity": "industrial level", "img": "fiveseven-violent-daimyo.png" }
+    ],
+    "clutch": [
+        {"key": "key.png"},
+        {"boxName": "clutch"},
+        { "name": "Neo-Noir", "weapon": "M4A4", "rarity": "hidden", "img": "m4a4-neo-noir.png" },
+        { "name": "Bloodsport", "weapon": "MP7", "rarity": "hidden", "img": "mp7-bloodsport.png" },
+        { "name": "Mortis", "weapon": "AWP", "rarity": "exotic", "img": "awp-mortis.png" },
+        { "name": "Cortex", "weapon": "USP-S", "rarity": "exotic", "img": "usps-cortex.png" },
+        { "name": "Stymphalian", "weapon": "AUG", "rarity": "remarkable", "img": "aug-stymphalian.png" },
+        { "name": "Moonrise", "weapon": "Glock-18", "rarity": "remarkable", "img": "glock18-moonrise.png" },
+        { "name": "Arctic Wolf", "weapon": "UMP-45", "rarity": "remarkable", "img": "ump45-arctic-wolf.png" },
+        { "name": "SWAG-7", "weapon": "MAG-7", "rarity": "remarkable", "img": "mag7-swag7.png" },
+        { "name": "Lionfish", "weapon": "Negev", "rarity": "remarkable", "img": "negev-lionfish.png" },
+        { "name": "Wild Six", "weapon": "Nova", "rarity": "remarkable", "img": "nova-wild-six.png" },
+        { "name": "Grip", "weapon": "R8 Revolver", "rarity": "industrial level", "img": "r8revolver-grip.png" },
+        { "name": "Urban Hazard", "weapon": "P2000", "rarity": "industrial level", "img": "p2000-urban-hazard.png" },
+        { "name": "Black Sand", "weapon": "MP9", "rarity": "industrial level", "img": "mp9-black-sand.png" },
+        { "name": "Flame Test", "weapon": "Five-SeveN", "rarity": "industrial level", "img": "fiveseven-flame-test.png" },
+        { "name": "Aloha", "weapon": "SG 553", "rarity": "industrial level", "img": "sg553-aloha.png" },
+        { "name": "Night Riot", "weapon": "PP-Bizon", "rarity": "industrial level", "img": "ppbizon-night-riot.png" },
+        { "name": "Oxide Blaze", "weapon": "XM1014", "rarity": "industrial level", "img": "xm1014-oxide-blaze.png" }
+    ]
+}
+let inventory = document.getElementById("inventory")
+let inventoryBox = document.getElementById("inventoryBox")
+let boxesBox = document.getElementById("boxes")
+let caixa
+function callBox(value) {
+    let boxItems = document.getElementById('boxItems')
+    let boxBtn = document.getElementById('boxBtn')
+    boxItems.innerHTML = ""
+    boxBtn.innerHTML = ""
+
+    switch(value) {
+        case 'snakebite':
+          caixa = boxes.snakebite
+        break;
+        case 'falchion':
+          caixa = boxes.falchion
+        break;
+        case 'clutch':
+          caixa = boxes.clutch
+        break;
+        case 'hidra':
+          caixa = boxes.hidra
+        break;
+        case 'gamma':
+          caixa = boxes.gamma
+        break;
+        default:
+        caixa = boxes.snakebite
+    }
+
+    for (let i = 2; i < caixa.length; i++) {
+        boxItems.innerHTML += `
+        <div class="boxItems-item">
+            <img id="itemImg" class="boxItems-item-img" src="./assets/imgs/${value}/${caixa[i].img}">
+            <h4 id="itemName" class="boxItems-item-weapon">${caixa[i].weapon}</h4>
+            <h4 id="itemName" class="boxItems-item-name">${caixa[i].name}</h4>
+        </div>`
+    }
+
+    boxBtn.innerHTML = `
+    <img id="itemImg" class="boxBtn-img" src="./assets/imgs/${value}/${caixa[0].key}">
+    <h4 id="itemName" class="boxBtn-name">Usar chave da ${value}</h4>
+    <button id="itemBtn" class="boxBtn-btn" onclick="sortItems()">destrancar recipiente</button>`
+}
+function checkInventory() {
+    var inventoryItems = JSON.parse(localStorage.getItem("inventory"))
+    console.log(inventoryItems);
+
+
+    if (localStorage.hasOwnProperty("inventory")) {
+        console.log(inventoryItems);
+        
+
+        // inventory.innerHTML += `
+        // <div class="main-inventory-resp">
+        //     <h2 class="main-inventory-resp-title">Inventárioooo</h2>
+        //     <button class="main-inventory-resp-btn" onclick="callInventory()">fechar</button>
+        // </div>`
+
+        
+        inventoryItems.forEach(element => {
+            const item = element.split("_");
+            console.log(item);
+            let itemCase = item[0]
+            let itemWeapon = item[1]
+            let itemName = item[2]
+            let itemImg = item[3]
+            console.log(itemCase);
+            console.log(itemWeapon);
+            console.log(itemName);
+            console.log(itemImg);
+            
+            inventoryBox.innerHTML += `
+            <div class="main-inventory-item">
+                
+                <h3 class="main-inventory-resp-item-case">${itemCase}</h3>
+                <h3 class="main-inventory-resp-item-weapon">${itemWeapon}</h3>
+                <h3 class="main-inventory-resp-item-name">${itemName}</h3>
+                <img class="main-inventory-resp-item-img" src="./assets/imgs/${itemCase}/${itemImg}">
+
+            </div>`
+        });
+
+
+        
+    }else{
+        console.log('hasahus auhsahsuas')
+        inventory.innerHTML = `
+        <div class="main-inventory-resp">
+            <img class="main-inventory-resp-img" src="./assets/imgs/notFound.png">
+            <h3 class="main-inventory-resp-text">Você ainda não tem nenhum item no inventário. Abra uma caixa!</h3>
+            <button class="main-inventory-resp-btn" onclick="callInventory()">fechar</button>
+        </div>`
+    }
+
+
+
+
+}
+
+function callResp() {
+    resp.classList.toggle("active")
+}
+function callInventory() {
+    inventory.classList.toggle("active")
+    // boxesBox.classList.remove("active")
+    checkInventory()
+}
+function callBoxes() {
+    // boxesBox.classList.add("active")
+    inventory.classList.remove("active")
+}
+function sortItems() {
+    let n = Math.floor((Math.random() * caixa.length));
+    let resp = document.getElementById('resp')
+
+    let i = Math.floor((Math.random() * 3));
+    var status = [
+        { "status": "ta zuado" },
+        { "status": "ta novo parça" },
+        { "status": "ta meio riscado" }
+    ]
+    console.log(status[i].status);
+    var teste = status[i].status
+
+
+    if (n == 0 || n == 1) {
+        sortItems()
+    }else{
+        callResp()
+        resp.innerHTML = `
+        <button class="resp-btn" onclick="callResp()">close</button>
+        <h2 class="resp-text">parabéns iha, vc ganhou isso aqui ó<h2>
+        <img class="resp-img" src="assets/imgs/${caixa[1].boxName}/${caixa[n].img}"> 
+        <h3 class="resp-name">${caixa[n].weapon} - ${caixa[n].name}</h3> 
+        <h3 class="resp-name">Status: ${teste}</h3> 
+        `
+
+
+        // for (let i = 2; i < caixa.length; i++) {
+        //     resp.innerHTML += `
+        //     <img class="resp-img" src="assets/imgs/${caixa[1].boxName}/${caixa[i].img}">
+        //     `
+        //     // <img class="resp-img" src="assets/imgs/${caixa[1].boxName}/${caixa[n].img}">
+        // }
+
+
+        console.log(caixa, "caixa aqui");
+
+
+
+
+        addStorage(caixa, n)
+
+    }
+}
+let arrInventory = new Array()
+let arrTest = new Array()
+function addStorage(caixa, n) {
+    console.log("caixa sorteada" , caixa[n]);
+    let nameBox = caixa[1].boxName
+    let itemName = caixa[n].name
+    let itemImg = caixa[n].img
+    let itemWeapon = caixa[n].weapon
+    let item = `${nameBox}_${itemWeapon}_${itemName}_${itemImg}`
+    let testee
+    if (localStorage.hasOwnProperty("inventory")) {
+        testee = JSON.parse(localStorage.getItem("inventory"))
+        console.log(testee);
+        
+        for (let i = 0; i < testee.length; i++) {
+            // console.log(testee[i]);
+            arrTest.push(testee[i]);
+
+        }
+        
+        
+        
+        
+        // arrInventory.push(arrTest, item)
+        arrInventory.push(item)
+        localStorage.setItem("inventory", JSON.stringify(arrInventory))
+    }else{
+        arrInventory.push(item)
+        localStorage.setItem("inventory", JSON.stringify(arrInventory))
+    }
+
+    console.log(arrTest, "teste ta aqui mano");
+
+
+    // /**
+    //  * Verifica se a propriedade existe
+    //  * Caso exista, converte de String para Object
+    //  */
+    // if (localStorage.hasOwnProperty("peoples")) {
+    //   peoples = JSON.parse(localStorage.getItem("peoples"))
+    // }
+  
+    // /* Adiciona um novo valor no array criado */
+    // peoples.push({name: inputPeople.value})
+  
+    // /* Salva o item */
+    // localStorage.setItem("peoples", JSON.stringify(peoples))
+
+
+
+
+
+}
+
+
+$(document).ready(function () {
+    $('#boxes').slick({
+        dots: false,
+        infinite: true,
+        arrows: true,
+        speed: 300,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        centerMode: true
+    });
+});
